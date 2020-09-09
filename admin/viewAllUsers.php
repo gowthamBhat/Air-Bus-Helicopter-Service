3763eccfa9648b242b28fed8e7882a0cc852cbea
gowthamBhat <gowthambhat793@gmail.com>
2020-09-09 21:15:32 +0530

    minor changes in view all user

--- admin/viewAllUsers.php
+++ admin/viewAllUsers.php
@@ -32,7 +32,7 @@ if (!isset($_SESSION['admin_name'])) {
             <div class="container">
                 <div class="header-area">
 
-                    <a class="brand-title" href="http://localhost/hal/admin/admin-home.php">AIR BUS HELICOPTER SERVICE</a>
+                    <a class="brand-title" href="http://localhost/hal/admin/admin-home.php">AIRBUS HELICOPTER SERVICE</a>
 
                     <div class="header-menu" id="myNavbar">
                         <span class="aname_header"><?php echo $a_name; ?></span>
@@ -79,7 +79,7 @@ if (!isset($_SESSION['admin_name'])) {
 
                                 while ($row = mysqli_fetch_array($res)) {
                                     echo "<tr>";
-                                    echo "<td>" . $row['fname'] . "</td>";
+                                    echo "<td>" . $row['uname'] . "</td>";
                                     echo "<td>" . $row['email'] . "</td>";
                                     echo "<td>" . $row['mobile'] . "</td>";
 
